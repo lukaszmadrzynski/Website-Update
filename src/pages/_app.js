31264f7bda2f022d35e8980497004505b0160bae
lukaszmadrzynski <lukasz.madrzynski@yahoo.com>
2025-04-19 12:09:06 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,25 @@
+import Head from 'next/head';
 import '../css/main.css';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+  return (
+    <>  {/* Use Fragments to wrap multiple elements */}
+      <Head>
+        {/* --- START: Add Website Structured Data --- */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "name": "Cloud Mountain", // Your preferred site name
+            "url": "https://cloudmountain.top/" // Your homepage URL
+          }) }}
+        />
+        {/* --- END: Add Website Structured Data --- */}
+      </Head>
+
+      {/* This renders the current page */}
+      <Component {...pageProps} />
+    </>
+  );
 }
