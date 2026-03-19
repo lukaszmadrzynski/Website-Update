6e011dffa48f77a5e3e5530758b73ce0e1887ba6
lukaszmadrzynski <lukasz.madrzynski@yahoo.com>
2026-03-19 21:10:37 +0800

    Add custom webpack configuration for server caching

--- next.config.js
+++ next.config.js
@@ -6,7 +6,13 @@ const nextConfig = {
         stackbitPreview: process.env.STACKBIT_PREVIEW
     },
     trailingSlash: true,
-    reactStrictMode: true
+    reactStrictMode: true,
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.cache = false;
+        }
+        return config;
+    }
 };
 
 module.exports = nextConfig;
