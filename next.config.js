836252da7b6320b48b6b8800d714ccc78bb2958c
lukaszmadrzynski <lukasz.madrzynski@yahoo.com>
2026-03-19 22:22:37 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,8 @@ const nextConfig = {
     webpack: (config, { isServer }) => {
         config.cache = false;
         return config;
-    }
+    },
+    output: 'export'
 };
 
 module.exports = nextConfig;
