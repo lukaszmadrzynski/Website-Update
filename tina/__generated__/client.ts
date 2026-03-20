81ada080291630bf42e9844e4cfb14467a40ee07
lukaszmadrzynski <lukasz.madrzynski@yahoo.com>
2026-03-20 21:24:07 +0800

    Add files via upload

---
+++ tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: 'I:/3. Packaging/Website/Website Update-CloudFlare-TinaCMS/Website-Update/tina/__generated__/.cache/1774008261437', url: 'https://content.tinajs.io/2.2/content/ecde188b-d144-45bd-b59a-a8d20a7c4ab6/github/main', token: 'eee61040ad8327fe879d1da05bd77c3d683573eb', queries,  });
+export default client;
+  
