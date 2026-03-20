2385b835a1eb090c59e503dd829df40c1d0d3e3e
lukaszmadrzynski <lukasz.madrzynski@yahoo.com>
2026-03-20 14:32:02 +0800

    Update admin.tsx

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,7 +1,9 @@
-import { TinaAdmin, wrapTinaAdmin } from 'tinacms'
-
-const App = wrapTinaAdmin(TinaAdmin)
-
 export default function Admin() {
-  return <App />
+  return (
+    <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>TinaCMS Admin</h1>
+      <p>Visit the homepage and click the "Edit with Tina" button to edit content.</p>
+      <p>Or go to <a href="/">Homepage</a></p>
+    </div>
+  )
 }
