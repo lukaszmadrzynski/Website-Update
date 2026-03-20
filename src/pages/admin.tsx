b26159a6bb73634439fcf2baea5e75986fce5698
lukaszmadrzynski <lukasz.madrzynski@yahoo.com>
2026-03-20 14:28:46 +0800

    Update admin.tsx

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,5 +1,7 @@
-import { TinaAdmin } from 'tinacms'
+import { TinaAdmin, wrapTinaAdmin } from 'tinacms'
+
+const App = wrapTinaAdmin(TinaAdmin)
 
 export default function Admin() {
-  return <TinaAdmin />
+  return <App />
 }
